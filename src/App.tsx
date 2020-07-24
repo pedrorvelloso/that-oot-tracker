051ce8f9d4dbb3fe5b7baf74f13e7d097d19650f
Pedro Reis <pedro_reis14@hotmail.com>
2020-07-24 11:12:06 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      Helloa
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('root'))
---
+++ webpack/electron.webpack.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+const rootPath = path.resolve(__dirname, '..')
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  devtool: 'source-map',
+  entry: path.resolve(rootPath, 'electron', 'main.ts'),
+  target: 'electron-main',
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  node: {
+    __dirname: false
+  },
+  output: {
+    path: path.resolve(rootPath, 'dist'),
+    filename: '[name].js'
+  }
+}
